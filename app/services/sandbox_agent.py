437bd2f5f655b620b1a5ed5e74392f8bf9dc9fd4
NeoMody <neomody77@gmail.com>
2026-01-03 15:12:28 +0800

    fix: Use space separator for OAuth scope parsing

--- app/services/sandbox_agent.py
+++ app/services/sandbox_agent.py
@@ -563,10 +563,10 @@ else:
                 parsed = urllib.parse.urlparse(output)
                 params = urllib.parse.parse_qs(parsed.query)
                 if 'scope' in params:
-                    scopes = params['scope'][0].split('+')
+                    scopes = params['scope'][0].split(' ')
                     # 过滤掉 org:create_api_key scope
                     scopes = [s for s in scopes if s != 'org:create_api_key']
-                    params['scope'] = ['+'.join(scopes)]
+                    params['scope'] = [' '.join(scopes)]
                     new_query = urllib.parse.urlencode(params, doseq=True)
                     output = urllib.parse.urlunparse(parsed._replace(query=new_query))
                 logger.info(f"Got login URL: {output[:50]}...")
